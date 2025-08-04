451db2703948ce7f9d22c6789d1ccbd2706133a2
Oleksandr Palilov <opalilovit@gmail.com>
2025-08-04 21:33:05 +0300

    create http method post,patch,delete

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,10 +2,33 @@ import { ContactsCollection } from '../db/models/contact.js';
 
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
+
   return contacts;
 };
 
 export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
+
+  return contact;
+};
+
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactId, payload) => {
+  const contactUpdate = await ContactsCollection.findByIdAndUpdate(
+    { _id: contactId },
+    payload,
+    { new: true },
+  );
+
+  return contactUpdate;
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await ContactsCollection.findOneAndDelete({ _id: contactId });
+
   return contact;
 };
